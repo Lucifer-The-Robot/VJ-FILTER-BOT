edf58482dd55c1e0c9f136813e5d0d6efb639925
──「 ʟᴜᴄɪғᴇʀ 」── <nimovie09@gmail.com>
2024-12-16 08:32:08 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -30,11 +30,9 @@ async def start(client, message):
         buttons = [[
                 InlineKeyboardButton('Hᴇʟᴘ 🔧', callback_data="help"),
                 InlineKeyboardButton('🎭 Mᴏᴠɪᴇ Gʀᴏᴜᴘ', url=GRP_LNK)
-            ],[
-                InlineKeyboardButton('Tʀᴇɴᴅɪɴɢ ⚡', callback_data='trending'),
+          ],[
                 InlineKeyboardButton('Aʙᴏᴜᴛ 🦋', callback_data='about')
-            ],[
-                InlineKeyboardButton('ᴅɪsᴄʟᴀɪᴍᴇʀ', url="https://telegra.ph/DISCLAIMER-11-20-32")
+        ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup, disable_web_page_preview=True)
         await asyncio.sleep(2) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
